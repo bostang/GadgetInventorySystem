759dbb276c886aae0f87f2d7aafbb4a00af90f7a
darisnurhakim <darisnurhakim12@gmail.com>
2021-04-18 12:00:15 +0700

    tinggal split()

--- F03.py
+++ F03.py
@@ -1,31 +1,62 @@
-# Program cariRarity
+"""
+Pencari gadget berdasarkan rarity
+Akses: Admin, User
 
-# Kontributor : Muhammad Daris Nurhakim [16520170], ...
+Pengguna dapat mencari gadget dengan rarity tertentu. Pengguna akan memasukkan 
+rarity(C,B,A,S), kemudian aplikasi akan menampilkan Gadget dengan rarity tersebut.
 
+Catatan: Jenis rarity yang di input pasti valid(C,B,A,S).
+
+Desainer : 
+1. Muhammad Daris Nurhakim (16520170)
+2. 
+
+Coder :
+1. Muhammad Daris Nurhakim (16520170)
+2. 
+
+Tester :
+1.
+2.
+
+Tentang cariRarity:
+1. Sudah berjalan dengan baik
+2. Masih menggunakan .split()
+3. Belum di cek saat bergabung dengan semua fungsi
+
+"""
+
+# mengimport file dari Python
 import os
 
 def clear_screen():
+    # fungsi ini digunakan untuk membuat efek clear screen pada Python
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def isRarityinDatabase(rarity,database):
+    # fungsi ini digunakan untuk mengecek apakah rarity yang diinginkan ada di database atau tidak
     for element in database:
-        if rarity == element[4]:
+        if rarity == element[4]: # element[4] berisi rarity yang ada di database
             return True
     return False
 
 def convert_line_to_data(line):
+    # fungsi ini digunakan menulis data per baris dalam csv ke dalam bentuk array
     raw_array_of_data = line.split(",")
     array_of_data = [data.strip() for data in raw_array_of_data]
     return array_of_data
 
 def infoBarang(rarity,spek):
-    if (rarity == 'S') or (rarity == 'A') or (rarity == 'B') or (rarity == 'C'):
+    # fungsi ini digunakan untuk mendapatkan informasi mengenai barang yang ada di database
+    if (rarity == 'S') or (rarity == 'A') or (rarity == 'B') or (rarity == 'C'): # jika rarity valid
         arrayProcess = _gadget
     else:
         arrayProcess = []
     
+    # mengecek apakah rarity yang diinginkan ada di database atau tidak
     if isRarityinDatabase(rarity,arrayProcess):
         i = 0
+        # mengecek baris per baris
         while arrayProcess[i]:
             if spek == 'id':
                 return baris[0]
@@ -43,36 +74,44 @@ def infoBarang(rarity,spek):
     else:
         return "\nbarang tidak ditemukan di database"
 
+
+'''''''''''
+'ALGORITMA'
+'''''''''''
+# membaca file database .csv
 g = open("gadget.csv","r")
 raw_lines_g = g.readlines()
 g.close()
 lines_g = [raw_line.replace("\n", "") for raw_line in raw_lines_g]
 
+# mengakses database dalam bentuk array
 _gadget = []
-
 for line in lines_g:
     array_of_data = convert_line_to_data(line)
     _gadget.append(array_of_data)
 
+# Melakukan skema pencarian rarity
 clear_screen()
 database = _gadget
 print(">>> Pencarian Gadget berdasarkan rarity\n")
 rarity = input("Masukkan rarity: ")
 
+# jika inputan salah, maka akan diulang
 while (rarity != 'S') and (rarity != 'A') and (rarity != 'B') and (rarity != 'C'):
     print("\nMasukan Anda salah!\n")
     rarity = input("Masukkan rarity: ")
 
 print("\nHasil pencarian:")
+# mengecek apakah rarity ada di database atau tidak
 if isRarityinDatabase(rarity,database): 
     for baris in database:
-        if baris[4] == rarity: 
+        if baris[4] == rarity: # baris[4] berisi data tentang rarity
             print(f"\nNama            : {infoBarang(rarity,'nama')}")
             print(f"Deskripsi       : {infoBarang(rarity,'deskripsi')}")
             print(f"Jumlah          : {infoBarang(rarity,'jumlah')}")
             print(f"Rarity          : {infoBarang(rarity,'rarity')}")
             print(f"Tahun ditemukan : {infoBarang(rarity,'tahun_ditemukan')}")
-
+            
     print("\nSemua data telah ditampilkan")
             
 else:
